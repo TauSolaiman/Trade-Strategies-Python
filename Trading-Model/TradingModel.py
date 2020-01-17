6d4d801acdf16801c96113468a1eaeb7e44ffeab
Tausif Solaiman <tausifsolaiman@gmail.com>
2020-01-17 14:22:23 -0500

    calculated moving averages & graphed with plotly

--- Trading-Model/TradingModel.py
+++ Trading-Model/TradingModel.py
@@ -2,6 +2,11 @@ import pandas as pd
 import requests
 import json
 
+from pyti.smoothed_moving_average import smoothed_moving_average as sma
+
+from plotly.offline import plot
+import plotly.graph_objs as go
+
 
 class TradingModel:
 
@@ -9,6 +14,7 @@ class TradingModel:
     self.symbol = symbol
     self.df = self.getData()
 
+  # define URL
   def getData(self):
     base = 'https://api.binance.com'
     endpoint = '/api/v3/klines'
@@ -16,24 +22,68 @@ class TradingModel:
 
     url = base + endpoint+ params
 
+  #download data
     data = requests.get(url)
     dictionary = json.loads(data.text)
 
+  # put in dataframe
     df = pd.DataFrame.from_dict(dictionary)
     df = df.drop(range(6, 12), axis=1)
 
+  #rename columns
     col_names = ['time', 'open', 'high', 'low', 'close', 'volume']
     df.columns = col_names
 
-    print(df)
+  #converting strings to float
+    for col in col_names:
+      df[col] = df[col].astype(float)
 
+  # add moving averages
+    df['fast_sma'] = sma(df['close'].tolist(), 10)
+    df['slow_sma'] = sma(df['close'].tolist(), 30)
+
+    print(df)
+    
     return df
+
+  def plotData(self):
+      df = self.df
+
+      # plot candlestick chart
+      candle = go.Candlestick(
+        x = df['time'],
+        open = df['open'],
+        close = df['close'],
+        high = df['high'],
+        low = df['low'],
+        name = "Candlesticks")
+
+      # plot MAs
+      fsma = go.Scatter(
+        x = df['time'],
+        y = df['fast_sma'],
+        name = 'Fast SMA',
+        line = dict(color = ('rgba(102, 207, 255, 50)')))
+
+      ssma = go.Scatter(
+        x = df['time'],
+        y = df['slow_sma'],
+        name = 'Slow SMA',
+        line = dict(color = ('rgba(255, 207, 102, 50)')))
+
+      data = [candle, ssma, fsma]
+
+      #style & display
+      layout = go.Layout(title = self.symbol)
+      fig = go.Figure(data = data, layout = layout)
+
+      plot(fig, filename = self.symbol)
 
 
 def Main():
     symbol = "BTCUSDT"
     model = TradingModel(symbol)
-    model.getData()
+    model.plotData()
 
 if __name__== '__main__':
     Main()
