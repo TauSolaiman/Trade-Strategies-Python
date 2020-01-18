2cc25816aa3851bc7f516df2ad880d0ae2a4f847
Tausif Solaiman <tausifsolaiman@gmail.com>
2020-01-18 16:49:45 -0500

    bug fixes

--- Trading-Model/Binance.py
+++ Trading-Model/Binance.py
@@ -1,199 +1,204 @@
-import pandas as pd
-import requests
+import requests 
 import json
 import decimal
 import hmac
 import time
+import pandas as pd
 
 binance_keys = {
-  'api_key': "PASTE API KEY HERE",
-  'secret_key': "PASTE SECRET KEY HERE"
+	'api_key': "PASTE API KEY HERE",
+	'secret_key': "PASTE SECRET KEY HERE"
 }
 
-
 class Binance:
-  def __init__(self):
+	def __init__(self):
 
-    self.base = 'https://api.binance.com'
+		self.base = 'https://api.binance.com'
 
-    self.endpoints = {
-      "order": 'api/v1/order',
-      "testOrder": 'api/v1/order/test',
-      "allOrders": 'api/v1/allOrders',
-      "klines": 'api/v1/klines',
-      "exchangeInfo": 'api/v1/exchangeInfo'
-    }
+		self.endpoints = {
+			"order": '/api/v1/order',
+			"testOrder": '/api/v1/order/test',
+			"allOrders": '/api/v1/allOrders',
+			"klines": '/api/v1/klines',
+			"exchangeInfo": '/api/v1/exchangeInfo'
+		}
 
-  def GetTradingSymbols(self):
-    # Get all symbols that are currently tradeable
-    url = self.base + self.endpoints["exchangeInfo"]
+	def GetTradingSymbols(self):
+		''' Gets All symbols which are tradable (currently) '''
+		url = self.base + self.endpoints["exchangeInfo"]
 
-    try: 
-      response = requests.get(url)
-      data = json.loads(response.text)
-    except Exception as e:
-      print('Exception ocurred while trying to access'+url)
-      print(e)
-      return []
+		try: 
+			response = requests.get(url)
+			data = json.loads(response.text)
+		except Exception as e:
+			print(" Exception occured when trying to access "+url)
+			print(e)
+			return []
 
-    symbols_list = []
+		symbols_list = []
 
-    for pair in data['symbols']:
-      if pair['status'] == 'TRADING':
-        symbols_list.append(pair['symbol'])
+		for pair in data['symbols']:
+			if pair['status'] == 'TRADING':
+				symbols_list.append(pair['symbol'])
 
-    return symbols_list
+		return symbols_list
 
-  #Get data for a single trading pair
-  def GetSymbolData(self, symbol:str, interval:str):
+	def GetSymbolData(self, symbol, interval):
+		''' Gets trading data for one symbol '''
 
-    params = '?&symbol='+symbol+'&interval='+interval
+		params = '?&symbol='+symbol+'&interval='+interval
 
-    url = self.base + self.endpoints['klines'] + params
+		url = self.base + self.endpoints['klines'] + params
 
-  #download data
-    data = requests.get(url)
-    dictionary = json.loads(data.text)
+		# download data
+		data = requests.get(url)
+		dictionary = json.loads(data.text)
 
-  # put in dataframe
-    df = pd.DataFrame.from_dict(dictionary)
-    df = df.drop(range(6, 12), axis=1)
+		# put in dataframe and clean-up
+		df = pd.DataFrame.from_dict(dictionary)
+		df = df.drop(range(6, 12), axis=1)
 
-  #rename columns
-    col_names = ['time', 'open', 'high', 'low', 'close', 'volume']
-    df.columns = col_names
+		# rename columns
+		col_names = ['time', 'open', 'high', 'low', 'close', 'volume']
+		df.columns = col_names
 
-  #converting strings to float
-    for col in col_names:
-      df[col] = df[col].astype(float)
+		# transform values from strings to floats
+		for col in col_names:
+			df[col] = df[col].astype(float)
 
-    return df
-
-  def PlaceOrder(self, symbol:str, side:str, type:str, quantity:float, price:float, test:bool=True):
-    '''
-      Symbol: ETHBTC
-      ETH - base asset (what we buy for)
-      BTC - quote asset (what we sell for)
-      quantity - how much ETH we want 
-      price - how much BTC we're willing to sell it for
-    '''
-    params = {
-      'symbol': symbol,
-      'side': side,           # BUY OR SELL
-      'type': type,           # MARKET, LIMIT, STOP LOSS etc. 
-      'timeInForce': 'GTC',
-      'quantity': quantity,
-      'price': price.floatToString(price),
-      'recvWindow': 5000,
-      'timestamp': int(round(time.time()*1000))
-    }
-
-    self.signRequest(params)
-
-    url = ''
-    if test:
-      url = self.base + self.endpoints['testOrder']
-    else:
-      url = self.base + self.endpoints['order']
-
-    try:
-      response = requests.post(url, params=params, headers={"X-MBX-APIKEY": binance_keys['api_key']})
-    except Exception as e:
-      print('Exception occured while trying to place order on'+url)
-      print(e)
-      response = {'code': '-1', 'msg':e}
-      return None
-
-    return json.loads(response.text)
-
-  def CancelOrder(self, symbol:str, orderId:str):
-    '''
-      Cancels the order on a symbol based on orderId
-    '''
-    params = {
-      'symbol': symbol,
-      'orderId': orderId,
-      'recvWindow': 5000,
-      'timestamp': int(round(time.time()*1000))
-    }
-
-    self.signRequest(params)
-
-    url = self.base + self.endpoints['order']
-
-    try:
-      response = requests.delete(url, params=params, headers={"X-MBX-APIKEY": binance_keys['api_key']})
-    except Exception as e:
-      print('Exception occured while trying to cancel order on'+url)
-      print(e)
-      response = {'code': '-1', 'msg':e}
-      return None
-
-  def GetOrderInfo(self, symbol:str, orderId:str):
-    '''
-      Get info about an order based on orderId
-    '''
-    params = {
-      'symbol': symbol,
-      'orderId': orderId,
-      'recvWindow': 5000,
-      'timestamp': int(round(time.time()*1000))
-    }
-
-    self.signRequest(params)
-
-    url = self.base + self.endpoints['order']
-
-    try:
-      response = requests.get(url, params=params, headers={"X-MBX-APIKEY": binance_keys['api_key']})
-    except Exception as e:
-      print('Exception occured while trying to get order info on'+url)
-      print(e)
-      response = {'code': '-1', 'msg':e}
-      return None
-
-    return json.loads(response.text)
-
-  def GetAllOrderInfo(self, symbol:str, orderId:str):
-    '''
-      Get info about an order based on orderId
-    '''
-    params = {
-      'symbol': symbol,
-      'timestamp': int(round(time.time()*1000))
-    }
-
-    self.signRequest(params)
-
-    url = self.base + self.endpoints['allOrders']
-
-    try:
-      response = requests.get(url, params=params, headers={"X-MBX-APIKEY": binance_keys['api_key']})
-    except Exception as e:
-      print('Exception occured while trying to get info on all orders'+url)
-      print(e)
-      response = {'code': '-1', 'msg':e}
-      return None
-
-    return json.loads(response.text)
+		return df
 
 
+	def PlaceOrder(self, symbol, side, type, quantity, price, test):
+
+		'''
+		Symbol: ETHBTC
+		ETH - base Asset (what we buy)
+		BTC - quote Asset (what we sell for)
+		quantity - how much ETH we want
+		price - how much BTC we're willing to sell it for
+		'''
+
+		params = {
+			'symbol': symbol,
+			'side': side, 			# BUY or SELL
+			'type': type,				# MARKET, LIMIT, STOP LOSS etc
+			'timeInForce': 'GTC',
+			'quantity': quantity,
+			'price' : self.floatToString(price),
+			'recvWindow': 5000,
+			'timestamp': int(round(time.time()*1000))
+		}
+
+		self.signRequest(params)
+
+		url = ''
+		if test: 
+			url = self.base + self.endpoints['testOrder']
+		else:
+			url = self.base + self.endpoints['order']
 
 
-# Converts float to string without resorting to scientific notation
-  def floatToString(self, f:float):
-    ctx = decimal.Context()
-    ctx.prec = 12
-    d1 = ctx.create_decimal(repr(f))
-    return format(d1, 'f')
+		try: 
+			response = requests.post(url, params=params, headers={"X-MBX-APIKEY": binance_keys['api_key']})
+		except Exception as e:
+			print(" Exception occured when trying to palce order on "+url)
+			print(e)
+			response = {'code': '-1', 'msg':e}
+			return None
 
-# Signs request to Binance API
-  def signRequest(self, params:dict):
-    query_string = '&'.join(["{}={}".format(d, params[d]) for d in params])
-    signature = hmac.new(binance_keys['secret_key'].encode('utf-8'), query_string.encode('utf-8'), hashlib.sha256)
-    params['signature'] = signature.hexdigest()
+		return json.loads(response.text)
+
+	def CancelOrder(self, symbol, orderId):
+		'''
+			Cancels the order on a symbol based on orderId
+		'''
+
+		params = {
+			'symbol': symbol,
+			'orderId' : orderId,
+			'recvWindow': 5000,
+			'timestamp': int(round(time.time()*1000))
+		}
+
+		self.signRequest(params)
+
+		url = self.base + self.endpoints['order']
+
+		try: 
+			response = requests.delete(url, params=params, headers={"X-MBX-APIKEY": binance_keys['api_key']})
+		except Exception as e:
+			print(" Exception occured when trying to cancel order on "+url)
+			print(e)
+			response = {'code': '-1', 'msg':e}
+			return None
+
+	def GetOrderInfo(self, symbol, orderId):
+		'''
+			Gets info about an order on a symbol based on orderId
+		'''
+
+		params = {
+			'symbol': symbol,
+			'orderId' : orderId,
+			'recvWindow': 5000,
+			'timestamp': int(round(time.time()*1000))
+		}
+
+		self.signRequest(params)
+
+		url = self.base + self.endpoints['order']
+
+		try: 
+			response = requests.get(url, params=params, headers={"X-MBX-APIKEY": binance_keys['api_key']})
+		except Exception as e:
+			print(" Exception occured when trying to get order info on "+url)
+			print(e)
+			response = {'code': '-1', 'msg':e}
+			return None
+
+		return json.loads(response.text)
 
 
+	def GetAllOrderInfo(self, symbol):
+		'''
+			Gets info about all order on a symbol
+		'''
+
+		params = {
+			'symbol': symbol,
+			'timestamp': int(round(time.time()*1000))
+		}
+
+		self.signRequest(params)
+
+		url = self.base + self.endpoints['allOrders']
+
+		try: 
+			response = requests.get(url, params=params, headers={"X-MBX-APIKEY": binance_keys['api_key']})
+		except Exception as e:
+			print(" Exception occured when trying to get info on all orders on "+url)
+			print(e)
+			response = {'code': '-1', 'msg':e}
+			return None
+
+		return json.loads(response.text)
+
+	def floatToString(self, f):
+		''' Converts the given float to a string,
+		without resorting to the scientific notation '''
+
+		ctx = decimal.Context()
+		ctx.prec = 12
+		d1 = ctx.create_decimal(repr(f))
+		return format(d1, 'f')
+
+	def signRequest(self, params):
+		''' Signs the request to the Binance API '''
+
+		query_string = '&'.join(["{}={}".format(d, params[d]) for d in params])
+		signature = hmac.new(binance_keys['secret_key'].encode('utf-8'), query_string.encode('utf-8'), hashlib.sha256)
+		params['signature'] = signature.hexdigest()
 
 
 
--- Trading-Model/TradingModel.py
+++ Trading-Model/TradingModel.py
@@ -2,151 +2,154 @@ import pandas as pd
 import requests
 import json
 
-from plotly.offline import plot
 import plotly.graph_objs as go
+from plotly.offline import plot
 
 from pyti.smoothed_moving_average import smoothed_moving_average as sma
 from pyti.bollinger_bands import lower_bollinger_band as lbb
 
-#Using Binance Class to download data for a symbol
-from Binance import Binance 
+#Using Binance class to grab symbol data
+from Binance import Binance
 
 class TradingModel:
+	
+	def __init__(self, symbol):
+		self.symbol = symbol
+		self.exchange = Binance()
+		self.df = self.exchange.GetSymbolData(symbol, '4h')
+		self.last_price = self.df['close'][len(self.df['close'])-1]
+		self.buy_signals = []
 
-  def __init__(self, symbol):
-    self.symbol = symbol
-    self.exchange = Binance()
-    self.df = self.exchange.GetSymbolData(symbol, '1h')
-    self.last_price = self.df['close'][len(self.df['close'])-1]
-    self.buy_signals = []
+		try:
+			self.df['fast_sma'] = sma(self.df['close'].tolist(), 10)
+			self.df['slow_sma'] = sma(self.df['close'].tolist(), 30)
+			self.df['low_boll'] = lbb(self.df['close'].tolist(), 14)
+		except Exception as e:
+			print(" Exception raised when trying to compute indicators on "+self.symbol)
+			print(e)
+			return None
 
-    try:
-      # add moving averages and lower bollinger band
-      self.df['fast_sma'] = sma(self.df['close'].tolist(), 10)
-      self.df['slow_sma'] = sma(self.df['close'].tolist(), 30)
-      self.df['low_boll'] = lbb(self.df['close'].tolist(), 14)
-    except Exception as e:
-      print('Exception raised when trying to computer indicators on'+ self.symbol)
-      print(e)
-      return None
+	def strategy(self):	
+		
+		'''If Price is 3% below Slow Moving Average, then add Buy
+	  	Add sell signal for 2% above buying price'''
+
+		df = self.df
+
+		buy_signals = []
+
+		for i in range(1, len(df['close'])):
+			if df['slow_sma'][i] > df['low'][i] and (df['slow_sma'][i] - df['low'][i]) > 0.03 * df['low'][i]:
+				buy_signals.append([df['time'][i], df['low'][i]])
+
+		self.plotData(buy_signals = buy_signals)
+
+	def plotData(self, buy_signals = False):
+		df = self.df
+
+		# plot candlestick chart
+		candle = go.Candlestick(
+			x = df['time'],
+			open = df['open'],
+			close = df['close'],
+			high = df['high'],
+			low = df['low'],
+			name = "Candlesticks")
+
+		# plot MAs
+		fsma = go.Scatter(
+			x = df['time'],
+			y = df['fast_sma'],
+			name = "Fast SMA",
+			line = dict(color = ('rgba(102, 207, 255, 50)')))
+
+		ssma = go.Scatter(
+			x = df['time'],
+			y = df['slow_sma'],
+			name = "Slow SMA",
+			line = dict(color = ('rgba(255, 207, 102, 50)')))
+
+		lowbb = go.Scatter(
+			x = df['time'],
+			y = df['low_boll'],
+			name = "Lower Bollinger Band",
+			line = dict(color = ('rgba(255, 102, 207, 50)')))
+
+		data = [candle, ssma, fsma, lowbb]
+
+		if buy_signals:
+			buys = go.Scatter(
+					x = [item[0] for item in buy_signals],
+					y = [item[1] for item in buy_signals],
+					name = "Buy Signals",
+					mode = "markers",
+				)
+
+			sells = go.Scatter(
+					x = [item[0] for item in buy_signals],
+					y = [item[1]*1.05 for item in buy_signals],
+					name = "Sell Signals",
+					mode = "markers",
+				)
+
+			data = [candle, ssma, fsma, buys, sells]
 
 
+		# style and display
+		layout = go.Layout(title = self.symbol)
+		fig = go.Figure(data = data, layout = layout)
 
-  # define URL
-  def getData(self):
-    base = 'https://api.binance.com'
-    endpoint = '/api/v3/klines'
-    params = '?&symbol='+self.symbol+'&interval=1h'
-
-    url = base + endpoint+ params
-
-  #download data
-    data = requests.get(url)
-    dictionary = json.loads(data.text)
-
-  # put in dataframe
-    df = pd.DataFrame.from_dict(dictionary)
-    df = df.drop(range(6, 12), axis=1)
-
-  #rename columns
-    col_names = ['time', 'open', 'high', 'low', 'close', 'volume']
-    df.columns = col_names
-
-  #converting strings to float
-    for col in col_names:
-      df[col] = df[col].astype(float)
-
-    print(df)
-    
-    return df
-
-  def strategy(self):
-    df = self.df
-
-    buy_signals = []
-
-    for i in range (1, len(df['close'])):
-      if df['slow_sma'][i] > df['close'][i] and (df['slow_sma'][i] - df['close'][i]) > 0.03 * df['close'][i]:
-        buy_signals.append([df['time'][i], df['low'][i]])
-
-    self.plotData(buy_signals = buy_signals)
-    
+		plot(fig, filename=self.symbol+'.html')
 
 
-  def plotData(self, buy_signals = False):
-      df = self.df
+	def maStrategy(self, i):
+		''' If price is 20% below the Slow MA, return True'''
 
-      # plot candlestick chart
-      candle = go.Candlestick(
-        x = df['time'],
-        open = df['open'],
-        close = df['close'],
-        high = df['high'],
-        low = df['low'],
-        name = "Candlesticks")
+		df = self.df
+		buy_price = 0.8 * df['slow_sma'][i]
+		if buy_price >= df['close'][i]:
+			self.buy_signals.append([df['time'][i], df['close'][i], df['close'][i] * 1.045])
+			return True
 
-      # plot MAs
-      fsma = go.Scatter(
-        x = df['time'],
-        y = df['fast_sma'],
-        name = 'Fast SMA',
-        line = dict(color = ('rgba(102, 207, 255, 50)')))
+		return False
 
-      ssma = go.Scatter(
-        x = df['time'],
-        y = df['slow_sma'],
-        name = 'Slow SMA',
-        line = dict(color = ('rgba(255, 207, 102, 50)')))
+	def bollStrategy(self, i):
+		''' If price is 2% below the Lower Bollinger Band, return True'''
 
-      data = [candle, ssma, fsma]
+		df = self.df
+		buy_price = 0.98 * df['low_boll'][i]
+		if buy_price >= df['close'][i]:
+			self.buy_signals.append([df['time'][i], df['close'][i], df['close'][i] * 1.045])
+			return True
 
-      if buy_signals:
-        buys = go.Scatter(
-          x = [item[0] for item in buy_signals],
-          y = [item[1] for item in buy_signals],
-          name = 'Buy Signals',
-          mode = "markers",
-        )
-        sells = go.Scatter(
-          x = [item[0] for item in buy_signals],
-          y = [item[1]*1.02 for item in buy_signals],
-          name = 'Sell Signals',
-          mode = "markers",
-        )
-      data = [candle, ssma, fsma, buys, sells]
-
-      #style & display
-      layout = go.Layout(title = self.symbol)
-      fig = go.Figure(data = data, layout = layout)
-
-      plot(fig, filename = self.symbol)
-
-# Moving average and Bollinger Band Strategy
-  def maStrategy(self, i:int):
-    ''' If price is 10% below slow moving average, put a buy signal'''
-    df = self.df
-    buy_price = 0.9 * df['slow_sma'][i]
-    if buy_price >= df['close'][i]:
-      self.buy_signals.append(df['time'], df['close'][i], df['close'][i] * 1.045)
-      return True
-    
-    return False
-
-  def bollStrategy(self, i:int):
-    ''' if price is 5% below lower bollinger band put a buy signal'''
-    df = self.df
-    buy_price = 0.95 * df['low_boll'][i]
-    if buy_price >= df['close'][i]:
-      self.buy_signals.append(df['time'], df['close'][i], df['close'][i] * 1.045)
-      return True
-    
-    return False    
+		return False
 
 
 def Main():
-    symbol = "BTCUSDT"
-    model = TradingModel(symbol)
-    model.strategy()
 
-if __name__== '__main__':
-    Main()
+    # symbol = "BTCUSDT"
+    # model = TradingModel(symbol)
+    # model.strategy()
+
+	exchange = Binance()
+	symbols = exchange.GetTradingSymbols()
+	for symbol in symbols:
+
+		print(symbol)
+		model = TradingModel(symbol)
+		plot = False
+		
+		if model.maStrategy(len(model.df['close'])-1):
+			print(" MA Strategy match on "+symbol)
+			plot = True
+
+		if model.bollStrategy(len(model.df['close'])-1):
+			print(" Boll Strategy match on "+symbol)
+			plot = True
+
+		if plot:
+			model.plotData()
+
+if __name__ == '__main__':
+	Main()
+
