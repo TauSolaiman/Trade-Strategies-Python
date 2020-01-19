b9e7d801c9200a6abf73bff020ce6846e1bf5f5e
Tausif Solaiman <tausifsolaiman@gmail.com>
2020-01-19 16:53:41 -0500

    helper functions, bug fixes

--- Trading-Model/StrategyEvaluator.py
+++ Trading-Model/StrategyEvaluator.py
@@ -59,6 +59,7 @@ class StrategyEvaluator:
 
     resulting_balance = Decimal(starting_balance)
     stop_loss = Decimal(initial_stop_loss)
+    profit_target = Decimal(initial_profits)
     buy_price = 0
     
     #Go through all the candlesticks
@@ -75,15 +76,17 @@ class StrategyEvaluator:
           last_buy = {
             "index" = i,
             "price" = buy_price
-            "desired_profits" = Decimal(initial_profits)
           }
           buy_times.append([df['date'][i], buy_price])
+
+          stop_loss = Decimal(initial_stop_loss)
+          profit_target = Decimal(initial_profits)
 
         elif last_buy is not None and i > last_buy['index']-1:
           # If we already bought, check whether the price has hit
           # either the stop loss price or the target price
           stop_loss_price = last_buy["price"] * stop_loss
-          next_target_price = last_buy["price"] * last_buy["desired_profts"]
+          next_target_price = last_buy["price"] * profit_target
 
           #If price goes below our stop loss, we sell at this point
           if df['low'] < stop_loss_price:
@@ -98,14 +101,14 @@ class StrategyEvaluator:
             last_buy = {
               "index" = i,
               "price" = Decimal(next_target_price),
-              "profits" = Decimal(incremental_profits)
             }
 
             stop_loss = Decimal(incremental_stop_loss)
+            profit_target = Decimal(incremental_profits)
 
       # Aggregate results and add them to this model's symbol
       self.results[model.symbol] = dict(
-        returns = round(Decimal(100) * (resulting_balance/starting_balance - Decimal(1.0)), 3),
+        returns = round(Decimal(100) * (resulting_balance/Decimal(starting_balance) - Decimal(1.0)), 3),
         buy_times = buy_times
         sell_times = sell_times
       )
@@ -117,5 +120,31 @@ class StrategyEvaluator:
 
       return resulting_balance
           
+  def evaluate(self, model):
+    last_entry = len(df['close']-1)
+    return self.strategy(model.df, last_entry)
+  
+  def updateResult(self, starting_balance, resulting_balance):
+    self.complete_starting_balance = self.complete_starting_balance + starting_balance
+    self.complete_resulting_balance = self.complete_resulting_balance + resulting_balance
+
+  def printResults(self):
+    print(self.strategy.__name__+"STATS:")
+    print("Profitable symbols: " + str(self.profitable_symbols))
+    print("Unprofitable symbols: " + str(self.unprofitable_symbols))
+
+    if len(self.profits_list) > 0:
+
+      profitability = round(Decimal(100.0) * self.complete_resulting_balance/self.complete_starting_balance - Decimal(1.0))
+
+      print("Overall Profits"+str(round(sum(self.profits_list), 2)))
+      print("Least Profitable Trade"+str(round(min(self.profits_list), 2)))
+      print("Most Profitable Trade"+str(round(max(self.profits_list), 2)))
+
+      print("With an initial balance of"+str(self.complete_starting_balance))+\
+        "and a final balance of"+str(self.complete_resulting_balance)
+      print("The profitability is "+str(round(profitability, 2))+"%")
+
+
 
 
