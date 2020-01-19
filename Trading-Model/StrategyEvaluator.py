0ba6e828cfbd3a08b91de5e518d886d39e1cdfe5
Tausif Solaiman <tausifsolaiman@gmail.com>
2020-01-19 14:30:14 -0500

    backtest function finished

--- Trading-Model/StrategyEvaluator.py
+++ Trading-Model/StrategyEvaluator.py
@@ -1,3 +1,7 @@
+import pandas as pd
+from decimal import Decimal, getcontext
+
+
 class StrategyEvaluator:
   ''' Used to evaluate the performance of strategies '''
 
@@ -26,6 +30,92 @@ class StrategyEvaluator:
    incremental_stop_loss = 0.975):
 
    '''
-    Funtion used to back test a strategy given a trading Model
+    Funtion used to backtest a strategy given a trading Model
+
+    parameters
+    --
+
+    starting balance 
+      The balance with which to start the strategy backtesting
+
+    etc
+    --
+    Returns
+      The balance after having run the strategy
    '''
+    if initial_stop_loss >=1 or initial_stop_loss <= 0:
+      AssertionError("Initial stop loss should be between 0 and 1!")
+
+    if initial_profits <=1:
+      AssertionError("Initial profits should be greater than one!")
+
+    df = model.df
+    buy_times = []
+    sell_times = []
+
+    last_buy = None
     
+    getcontext().prec = 30
+
+    resulting_balance = Decimal(starting_balance)
+    stop_loss = Decimal(initial_stop_loss)
+    buy_price = 0
+    
+    #Go through all the candlesticks
+    for i in range(0, len(df['close']-1):
+      # Have we alreadt bought? we don't want to do parallel trades on the same symbol
+      if last_buy is None:
+        #No then check whether the strategy is fullfilled at this time
+        strategy_result = self.strategy(model.df, i)
+
+        if strategy_result:
+          #If strategy is fullfilled, then buy
+          buy_price = Decimal(strategy_result)
+
+          last_buy = {
+            "index" = i,
+            "price" = buy_price
+            "desired_profits" = Decimal(initial_profits)
+          }
+          buy_times.append([df['date'][i], buy_price])
+
+        elif last_buy is not None and i > last_buy['index']-1:
+          # If we already bought, check whether the price has hit
+          # either the stop loss price or the target price
+          stop_loss_price = last_buy["price"] * stop_loss
+          next_target_price = last_buy["price"] * last_buy["desired_profts"]
+
+          #If price goes below our stop loss, we sell at this point
+          if df['low'] < stop_loss_price:
+            sell_times.append([df['date'], stop_loss_price])
+            resulting_balance = resulting_balance * (stop_loss_price / buy_price)
+
+            last_buy = None
+            buy_price = Decimal(0)
+
+          elif df['low'][i] >= next_target_price:
+            # If price goes above our target, we increased our stop loss and set next target
+            last_buy = {
+              "index" = i,
+              "price" = Decimal(next_target_price),
+              "profits" = Decimal(incremental_profits)
+            }
+
+            stop_loss = Decimal(incremental_stop_loss)
+
+      # Aggregate results and add them to this model's symbol
+      self.results[model.symbol] = dict(
+        returns = round(Decimal(100) * (resulting_balance/starting_balance - Decimal(1.0)), 3),
+        buy_times = buy_times
+        sell_times = sell_times
+      )
+
+      if resulting_balance > starting_balance:
+        self.profitable_symbols = self.profitable_symbols + 1
+      if resulting_balance < starting_balance:
+        self.unprofitable_symbols = self.unprofitable_symbols + 1
+
+      return resulting_balance
+          
+
+
